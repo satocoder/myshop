301069c11edd969862811511f3f773e104290de4
satocoder <satokarashi@gmail.com>
2021-02-07 02:24:13 +0330

    Category and product page isnertion

--- myshop/shop/models.py
+++ myshop/shop/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Category(models.Model):
     name = models.CharField(max_length=200,db_index=True)
     slug = models.SlugField(max_length=200,unique=True)
+
 
     class Meta:
         ordering = ('name',)
@@ -12,6 +14,10 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category',args=[self.slug])
+
 
 class Product(models.Model):
     category = models.ForeignKey(Category,related_name='products',on_delete=models.CASCADE)
@@ -31,5 +37,5 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
-
-        
+    def get_absolute_url(self):
+        return reverse('shop:product_detail',args=[self.id, self.slug])
